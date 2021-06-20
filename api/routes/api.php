90ea42231ed4b93f516ed9f359c366731dbe20aa
Ričards <ricards.grinbergs@outlook.com>
2021-06-20 13:47:32 +0300

    Removed custom authentication strategy

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,11 +10,3 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-use App\Http\Controllers\Auth\LoginController;
-
-Route::prefix('auth')->group(function () {
-    Route::post('authenticate', [LoginController::class, 'authenticate']);
-    Route::post('register', [LoginController::class, 'register']);
-    Route::post('revoke', [LoginController::class, 'revoke'])->middleware('auth:sanctum');
-});
