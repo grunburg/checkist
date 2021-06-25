34e673543a7a1676716dbb4a8a34f6bb09cddd05
Ričards <ricards.grinbergs@outlook.com>
2021-06-25 18:05:37 +0300

    Added parent task scope & subtask relationship

--- api/app/Models/Task.php
+++ api/app/Models/Task.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -12,9 +14,21 @@ class Task extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $with = ['subtasks'];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeParent(Builder $builder)
+    {
+        $builder->whereNull('parent_id');
+    }
+
+    public function subtasks(): HasMany
+    {
+        return $this->hasMany(Task::class, 'parent_id');
     }
 
     public function taskable(): MorphTo
